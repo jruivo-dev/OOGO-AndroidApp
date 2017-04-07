1d7d01ca14206922f7b3f43df67f265e08839597
Jose Ruivo <jruivo.dev@gmail.com>
2017-04-07 11:32:48 +0100

    Added Splash Screen

---
+++ app/src/main/java/com/jruivodev/oogo/Splash.java
@@ -0,0 +1,39 @@
+package com.jruivodev.oogo;
+
+/**
+ * Created by Jojih on 07/04/2017.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity {
+
+    /**
+     * Duration of wait
+     **/
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_splashscreen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this, FirstScreenActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
