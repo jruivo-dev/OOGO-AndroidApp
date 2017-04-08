06f0c76f272e2d20e647d63baee976ec41094bea
Jose Ruivo <jruivo.dev@gmail.com>
2017-04-08 19:01:18 +0100

    Added JSONParser

--- app/src/main/java/com/jruivodev/oogo/LoginActivity.java
+++ app/src/main/java/com/jruivodev/oogo/LoginActivity.java
@@ -3,8 +3,16 @@ package com.jruivodev.oogo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.kosalgeek.genasync12.AsyncResponse;
+import com.kosalgeek.genasync12.PostResponseAsyncTask;
+
+import java.util.HashMap;
 
 
 /**
@@ -13,6 +21,8 @@ import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
     private final String LOG = "MainActivity";
+    private HashMap postData = new HashMap();
+    private EditText usernameEditText, passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +30,33 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button btnSignUp = (Button) findViewById(R.id.button_signup);
+
+
+        usernameEditText = (EditText) findViewById(R.id.text_login_username);
+        passwordEditText = (EditText) findViewById(R.id.text_login_password);
+        Button btnLogin = (Button) findViewById(R.id.button_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = usernameEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                postData.put("email", username);
+                postData.put("password", password);
+
+                PostResponseAsyncTask task1 = new PostResponseAsyncTask(LoginActivity.this, postData, new AsyncResponse() {
+                    @Override
+                    public void processFinish(String s) {
+                        Log.d(LOG, s);
+                        if (s.contains("success"))
+                            Toast.makeText(LoginActivity.this, "Success Login", Toast.LENGTH_LONG).show();
+                    }
+                });
+                task1.execute("http://10.0.3.2/2-login.php");
+            }
+        });
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,29 +67,5 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
-//        TextView userTypeText = (TextView) findViewById(R.id.text_login_type);
-//        ImageView userTypeImage = (ImageView) findViewById(R.id.image_user_type);
-//        if (userTypeString.equals("normal")) {
-//            userTypeImage.setImageResource(R.drawable.normal);
-//            userTypeText.setText("Normal Log In");
-//        }
-//
-//        if (userTypeString.equals("professional")) {
-//            userTypeImage.setImageResource(R.drawable.professional);
-//            userTypeText.setText("Professional Log In");
-//
-//        }
-
-//        HashMap postData = new HashMap();
-//        postData.put("email", "mail@gmail.com");
-//        postData.put("password", "pass");
-//
-//        PostResponseAsyncTask task1 = new PostResponseAsyncTask(this, postData, new AsyncResponse() {
-//            @Override
-//            public void processFinish(String s) {
-//                Log.d(LOG, s);
-//            }
-//        });
-//        task1.execute("http://10.0.3.2/2-login.php");
     }
 }
--- app/src/main/java/com/jruivodev/oogo/SignupActivity.java
+++ app/src/main/java/com/jruivodev/oogo/SignupActivity.java
@@ -2,15 +2,60 @@ package com.jruivodev.oogo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.kosalgeek.genasync12.AsyncResponse;
+import com.kosalgeek.genasync12.PostResponseAsyncTask;
+
+import java.util.HashMap;
 
 /**
  * Created by Jojih on 07/04/2017.
  */
 
 public class SignupActivity extends AppCompatActivity {
+    private final String LOG = "MainActivity";
+    private HashMap postData = new HashMap();
+    private EditText usernameEditText, passwordEditText, nameEditText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+
+        nameEditText = (EditText) findViewById(R.id.input_name);
+        usernameEditText = (EditText) findViewById(R.id.input_email);
+        passwordEditText = (EditText) findViewById(R.id.input_password);
+        Button btnCreateAccount = (Button) findViewById(R.id.button_create_account);
+
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+                String name = nameEditText.getText().toString().trim();
+
+                postData.put("email", username);
+                postData.put("password", password);
+                postData.put("nome", name);
+
+                PostResponseAsyncTask task1 = new PostResponseAsyncTask(SignupActivity.this, postData, new AsyncResponse() {
+                    @Override
+                    public void processFinish(String s) {
+                        Log.d(LOG, s);
+                        if (s.contains("Registo com sucesso"))
+                            Toast.makeText(SignupActivity.this, "Register Success", Toast.LENGTH_LONG).show();
+                    }
+                });
+                task1.execute("http://10.0.3.2/1-registo.php");
+            }
+        });
+
     }
 }
