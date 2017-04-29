540c570346a7523a3728fa023735c2a3ba632ed9
Jose Ruivo <jruivo.dev@gmail.com>
2017-04-29 12:29:40 +0100

    More visual changes.

--- app/src/main/java/com/jruivodev/oogo/UserProfileActivity.java
+++ app/src/main/java/com/jruivodev/oogo/UserProfileActivity.java
@@ -1,10 +1,14 @@
 package com.jruivodev.oogo;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RatingBar;
 
 import com.jruivodev.oogo.objects_and_adapters.Rating;
 import com.jruivodev.oogo.objects_and_adapters.RatingAdapter;
@@ -29,15 +33,20 @@ public class UserProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_profile);
 
 
-        ratings.add(new Rating("Johny", "Title1", "DDESCRIPTIN HELLO THANKS GOODBYE MY FRIENDS; LOVE YOU ALL"));
-        ratings.add(new Rating("Ana", "Title2", "DDESCRIPTIN HELLO THANKS GOODBYE MY FRIENDS; LOVE YOU ALL"));
-        ratings.add(new Rating("Marg", "Title3", "DDESCRIPTIN HELLO THANKS GOODBYE MY FRIENDS; LOVE YOU ALL"));
-        ratings.add(new Rating("Peter", "Title4", "DDESCRIPTIN HELLO THANKS GOODBYE MY FRIENDS; LOVE YOU ALL"));
+        ratings.add(new Rating("Johny", "Title1", "Ddescriptin hello thanks goodbye my friends; love you all"));
+        ratings.add(new Rating("Ana", "Title2", "Ddescriptin hello thanks goodbye my friends; love you all"));
+        ratings.add(new Rating("Marg", "Title3", "Ddescriptin hello thanks goodbye my friends; love you all"));
+        ratings.add(new Rating("Peter", "Title4", "Ddescriptin hello thanks goodbye my friends; love you all"));
 
 
         listView = (ListView) findViewById(R.id.rating_list_view);
         mAdapter = new RatingAdapter(this, ratings);
         listView.setAdapter(mAdapter);
+
+// Paints the rating stars color
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.rating_bar);
+        Drawable progress = ratingBar.getProgressDrawable();
+        DrawableCompat.setTint(progress, ContextCompat.getColor(this, R.color.colorPrimary));
 
         Button acceptBtn = (Button) findViewById(R.id.button_accept_user);
         if (acceptBtn != null)
--- app/src/main/java/com/jruivodev/oogo/objects_and_adapters/RatingAdapter.java
+++ app/src/main/java/com/jruivodev/oogo/objects_and_adapters/RatingAdapter.java
@@ -1,10 +1,14 @@
 package com.jruivodev.oogo.objects_and_adapters;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.jruivodev.oogo.R;
@@ -33,6 +37,11 @@ public class RatingAdapter extends ArrayAdapter<Rating> {
         }
 
         Rating currentRating = getItem(position);
+
+        // Paints the rating stars color
+        RatingBar ratingBar = (RatingBar) listRow.findViewById(R.id.rating_bar);
+        Drawable progress = ratingBar.getProgressDrawable();
+        DrawableCompat.setTint(progress, ContextCompat.getColor(getContext(), R.color.colorPrimary));
 
         TextView ratingTitle = (TextView) listRow.findViewById(R.id.rating_title);
         TextView ratingDescription = (TextView) listRow.findViewById(R.id.rating_description);
